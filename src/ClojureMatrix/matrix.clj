2ec4308575b43293486f0126e76ffeca2dd68279
Tyler Allen <takev.takev@gmail.com>
2012-03-14 22:18:23 -0700

    Added the ability to transpose a matrix.

--- src/ClojureMatrix/matrix.clj
+++ src/ClojureMatrix/matrix.clj
@@ -17,3 +17,17 @@
 
 (defn get-element [matrix row col]
 	(nth (get-row matrix row) col))
+
+(defn get-column [matrix col]
+	(map #(nth %1 col) matrix))
+
+(defn multiplicable? [matrixA matrixB]
+	"If true, returns a vector of the dimensions of the matrix that would result from multiplying the 
+	two matrices. Returns false otherwise."
+	(let [m (count (get-row matrixA)) 
+		  n (count (get-column matrixB))]
+		  (if (= m n) (list m n) nil)))
+
+(defn transpose [matrix]
+      (for [i (range (count (first matrix)))]
+	   (get-column matrix i)))
