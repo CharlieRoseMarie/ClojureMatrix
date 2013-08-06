665b402f8517ab109d2a12b6849f260a56740a1e
Tyler Allen <takev.takev@gmail.com>
2013-08-06 00:35:39 -0700

    Added preconditions to the rest of the functions.

--- src/ClojureMatrix/matrix.clj
+++ src/ClojureMatrix/matrix.clj
@@ -15,29 +15,34 @@
 		(create-identity-row n row)))
 
 (defn get-row [matrix row]
+  {:pre [(proper? matrix) (< row (count matrix))]}
 	(nth matrix row))
 
 (defn get-element [matrix row col]
+  {:pre [(proper? matrix)]}
 	(get-in matrix [row col]))
 
 (defn get-column [matrix col]
+  {:pre [(proper? matrix) (< col (count (first matrix)))]}
 	(map #(nth %1 col) matrix))
 
 (defn multiplicable? [matrixA matrixB]
 	"If true, returns a vector of the dimensions of the matrix that would result from multiplying the
 	two matrices. Returns nil otherwise."
+  {:pre [(proper? matrixA) (proper? matrixB)]}
 	(let [m (count (get-row matrixA))
 		  n (count (get-column matrixB))]
 		  (if (= m n) [m n] nil)))
 
 (defn square? [matrix]
-  {:pre (proper? matrix)}
   "Returns true if the matrix is square"
+  {:pre [(proper? matrix)]}
 	(when (proper? matrix)
 		(= (count (get-row matrix 0)) (count (get-column matrix 0)))))
 
 (defn transpose [matrix]
   "Returns the transpose of the matrix."
+  {:pre [(proper? matrix)]}
   (apply map vector matrix))
 
 
