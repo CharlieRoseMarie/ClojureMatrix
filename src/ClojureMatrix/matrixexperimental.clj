c541b7056753120e0d4d47b0c906bafbb30e58d9
Tyler Allen <takev.takev@gmail.com>
2013-08-26 02:37:58 -0700

    Added multiply, transpose, and matrix-to-nested. Also fixed multiplicable.

--- src/ClojureMatrix/matrixexperimental.clj
+++ src/ClojureMatrix/matrixexperimental.clj
@@ -1,6 +1,13 @@
-(ns ClojureMatrix.matrixexperimental)
+(ns ClojureMatrix.matrixexperimental
+  (:require [ClojureMatrix.vector :as v]))
 
 (defrecord Matrix [rows columns data])
+
+(defprotocol MatrixOperations
+  (valid-row?   [this row] "Returns true if the row is within the dimensions of the matrix")
+  (valid-column [this col] "Returns true if the col is within the dimensions of the matrix")
+  (get-element  [this row col] "Returns the element at row and col.")
+  (scalar-multiply [this value] "Multiplies the matrix by some value"))
 
 (defn valid-row? [matrix row]
   "Returns true if the row is within the dimensions of the matrix"
@@ -10,7 +17,6 @@
   "Returns true if the col is with the dimensions of the matrix"
   (and (>= col 0) (< col (:columns matrix))))
 
-
 (defn matrix-from-nested [matrix]
   (let [proper (apply = (map count matrix))
         rows (count matrix)
@@ -18,9 +24,13 @@
         data (vec (apply concat matrix))]
     (when proper (Matrix. rows cols data))))
 
+(defn matrix-to-nested [matrix]
+  "Turns a matrix object into a nested vector"
+  (vec (map vec (partition (:rows matrix) (:data matrix)))))
+
 ; Test values
-(def m1 (Matrix. 3 3 [1 0 1 0 1 2 0 0 1]))
-(def m2 (matrix-from-nested [[1 0 0 0][0 1 0 0][0 0 1 0 ]]))
+(def m1 (Matrix. 3 3 [1 0 1, 0 1 2, 0 0 1]))
+(def m2 (matrix-from-nested [[1 1 6 0][1 1 1 5][1 1 1 1]]))
 
 (defn get-element [matrix row col]
   {:pre [(valid-row? matrix row) (valid-column? matrix col)]}
@@ -32,19 +42,32 @@
   {:pre [(valid-row? matrix row)]}
   (let [start (* (:columns matrix) row)
         end   (* (:columns matrix) (inc row))]
-   (vec (subvec (:data matrix) star end))))
+   (vec (subvec (:data matrix) start end))))
 
 (defn get-column [matrix col]
   {:pre [(valid-column? matrix col)]}
-  (vec (take-nth (+ col (:row matrix)) (:data matrix))))
-
-(get-column m1 0)
+  (let [adjusted-data (drop col (:data matrix))]
+    (vec (take-nth (:columns matrix) adjusted-data))))
 
 (defn equal-dimensions? [m1 m2]
   "Returns true if the dimensions of the two matrices are equal"
   (and (= (:rows m1) (:rows m2))
        (= (:columns m1) (:columns m2))))
 
+(defn transpose [matrix]
+  "Returns the transpose of the matrix"
+  (let [new-row (:columns matrix)
+        new-col (:rows matrix)
+        new-data (apply concat(for [i (range new-row)] (get-column matrix i)))]
+    (Matrix. new-row new-col (vec new-data))))
+
+; Algebraic operations
+
+(defn multiplicable? [m1 m2]
+  "Returns nil if the two matrices can't be multiplied, otherwise returns the dimensions of the matrix that
+  would result"
+  (when (= (:rows m2) (:columns m1))
+    [(:rows m1) (:columns m2)]))
 
 (defn scalar-multiply [matrix value]
   (let [data (:data matrix)
@@ -71,3 +94,11 @@
    (let [initial (subtract m1 m2)]
      (reduce subtract initial args))))
 
+(defn multiply [m1 m2]
+  "Multiplies m1 and m2. Uses the naive algorithm"
+  (let [dimensions (multiplicable? m1 m2)]
+    (when (not (nil? dimensions))
+      (let [data (for [i (range (first dimensions))
+                       j (range (second dimensions))]
+                   (v/dot-product (get-row m1 i) (get-column m2 j)))]
+        (Matrix. (first dimensions) (second dimensions) (vec data))))))
